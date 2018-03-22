3ce0a77250bfb8f2d5ba942c2699011d68a604c2
Dino Arachchi <dino.arachchi@levvel.io>
2018-03-21 19:17:35 -0500

    Set up connection to local mongodb connection

--- server.js
+++ server.js
@@ -1,14 +1,32 @@
+// Dependencies
 var express = require("express");
 var bodyParser = require("body-parser");
-var path = require("path");
+var mongoose = require("mongoose");
 
 var app = express();
 var PORT = process.env.PORT || 8080;
 
+
+// Sets up the Express app to handle data parsing
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+// Database configuration with mongoose
+mongoose.connect("mongodb://localhost/f1-teams");
+
+var db = mongoose.connection;
+
+// Show any mongoose errors
+db.on("error", function(error) {
+	console.log("Mongoose Error: ", error);
+});
+
+// Once logged in to the db through mongoose, log a success message
+db.once("open", function() {
+	console.log("Mongoose connection successful.");
+});
 
 app.get("/", function(req, res) {
   res.sendFile(path.join(__dirname, "/public/index.html"));
