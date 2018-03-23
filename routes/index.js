201aca968200f1727b753d6e5a4bcfc735de98da
Dino Arachchi <dino.arachchi@levvel.io>
2018-03-23 17:36:03 -0500

    Logout button now clears cookie on Auth0 server as well

--- routes/index.js
+++ routes/index.js
@@ -30,8 +30,9 @@ router.get('/login', passport.authenticate('auth0', {
     res.render('login', { env: env});
 });
 
-router.get('/logout', function(req,res) {
-    req.logout();
+router.get('/logout', function (req, res){
+    var redirectUrl = 'https://f1-sso.auth0.com/v2/logout?returnTo=http%3A%2F%2Fwww.google.com&client_id=' + env.AUTH0_CLIENT_ID;
+
     res.redirect('/');
 });
 
