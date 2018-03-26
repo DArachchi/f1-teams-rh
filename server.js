fcda52b094436c3061911b44a1f11b613652e4c8
Dino Arachchi <dino.arachchi@levvel.io>
2018-03-26 13:46:30 -0500

    Added profile update time

--- server.js
+++ server.js
@@ -20,12 +20,15 @@ var strategy = new Auth0Strategy({
 	domain:       process.env.AUTH0_DOMAIN,
 	clientID:     process.env.AUTH0_CLIENT_ID,
 	clientSecret: process.env.AUTH0_CLIENT_SECRET,
-	callbackURL:  process.env.AUTH0_CALLBACK_URL
-}, function(accessToken, refreshToken, extraParams, profile, done) {
+	callbackURL:  process.env.AUTH0_CALLBACK_URL,
+	passReqToCallback: true
+}, function(req, accessToken, refreshToken, extraParams, profile, done) {
 	// accessToken is the token to call Auth0 API (not needed in the most cases)
 	// extraParams.id_token has the JSON Web Token
 	// profile has all the information from the user
-	return done(null, profile);
+	req.session.id_token = extraParams.id_token;
+	console.log(req.session)
+	return done(null, profile._json);
 });
 
 passport.use(strategy);
